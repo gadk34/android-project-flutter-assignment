62af1e4ee7448a6501eeefd53c09d3eb965042a5
gadi <gad.keller@campus.technion.ac.il>
2020-11-13 12:52:30 +0200

    Seperated userRepository to new file

---
+++ lib/user_repository.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
+import 'package:english_words/english_words.dart';
+
+enum Status { Uninitialized, Authenticated, Authenticating, Unauthenticated }
+
+class UserRepository with ChangeNotifier {
+  FirebaseAuth _auth;
+  User _user;
+  Status _status = Status.Uninitialized;
+  FirebaseFirestore _db;
+
+  UserRepository.instance() : _auth = FirebaseAuth.instance {
+    _auth.authStateChanges().listen(_authStateChanges);
+  }
+
+  Status get status => _status;
+
+  User get user => _user;
+
+  FirebaseAuth get auth => _auth;
+
+  FirebaseFirestore get firestore => _db;
+
+  Future<void> _addUser(DocumentReference userRef) async {
+    userRef.get().then((snapshot) {
+      if (!snapshot.exists) {
+        userRef.set({'email': _user.email, 'favorites': []});
+      }
+    });
+  }
+
+  Future<void> signIn(String email, String password) async {
+    try {
+      _status = Status.Authenticating;
+      notifyListeners();
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      _status = Status.Authenticated;
+      _db = FirebaseFirestore.instance;
+      await _addUser(_db.collection('users').doc(_user.email));
+      notifyListeners();
+    } catch (e) {
+      _status = Status.Unauthenticated;
+      notifyListeners();
+      throw e;
+    }
+  }
+
+  Future signOut() async {
+    _status = Status.Unauthenticated;
+    _auth.signOut();
+    _db = null;
+    notifyListeners();
+    return Future.delayed(Duration.zero);
+  }
+
+  Future syncSavedFavorites(Set saved) async {
+    if (status == Status.Authenticated) {
+      _db.collection('users').doc(_user.email).get().then((snapshot) async {
+        var currFaves = Set.from(snapshot.data()['favorites']);
+        currFaves.addAll(
+            saved.map<String>((f) => f.asPascalCase.toString()).toList());
+        await _db
+            .collection('users')
+            .doc(_user.email)
+            .update({'favorites': currFaves.toList()});
+      });
+      _db.collection('users').doc(_user.email).get().then((snapshot) async {
+        var currFaves = Set.from(snapshot.data()['favorites']);
+        saved = saved.union(currFaves);
+      });
+      notifyListeners();
+    }
+  }
+
+  Future addFavorite(WordPair pair, Set<WordPair> saved) async {
+    saved.add(pair);
+    if (status == Status.Authenticated) {
+      _db.collection('users').doc(_user.email).get().then((snapshot) async {
+        var currFaves = snapshot.data()['favorites'];
+        currFaves.add(pair.asPascalCase.toString());
+        await _db
+            .collection('users')
+            .doc(_user.email)
+            .update({'favorites': currFaves.toList()});
+        notifyListeners();
+      });
+    }
+  }
+
+  Future removeFavorite(WordPair pair, Set saved) async {
+    saved.remove(pair);
+    if (status == Status.Authenticated) {
+      _db.collection('users').doc(_user.email).get().then((snapshot) async {
+        var currFaves = Set<String>.from(snapshot.data()['favorites']);
+        currFaves.remove(pair.asPascalCase.toString());
+        await _db
+            .collection('users')
+            .doc(_user.email)
+            .update({'favorites': currFaves.toList()});
+        notifyListeners();
+      });
+    }
+  }
+
+  Future<void> _authStateChanges(User firebaseUser) async {
+    if (firebaseUser == null) {
+      _status = Status.Unauthenticated;
+    } else {
+      _user = firebaseUser;
+      _status = Status.Authenticated;
+    }
+  }
+}
